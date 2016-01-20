555efe5a9ddea5210a7b5389cb876e3c51ee8e34
Brandon Ellis <Brandon_Ellis@dhcp-10-202-124-163.tamulink.tamu.edu>
2016-01-20 11:44:31 -0600

    added help function

--- Ellis-Brandon-indiv-1.cpp
+++ Ellis-Brandon-indiv-1.cpp
@@ -7,13 +7,36 @@
 #include <iostream>
 using namespace std;
 
-int main(int argc, char *argv[] ){
-   
-    // figuring out c++ command line arguements
-    cout << argc << "\n";
-    for (int i = 1; i < argc; ++i) {
-        cout << argv[i] << "\n";
-    }
+
+
+void help(){
+    cout << "\t\tHELP:" << endl;
+    cout << "AddPlayer <Player ID> <Player Name>" << endl;
+    cout << "AddGame <Game ID> <Game Name>" << endl;
+    cout << "AddAchievement <Game ID> <Achievement ID> <Achievement Name> <Achievement Points>" << endl;
+    cout << "Plays <Player ID> <Game ID> <Player IGN>" << endl;
+    cout << "AddFriends <Player ID1> <Player ID2>" << endl;
+    cout << "Achieve <Player ID> <Game ID> <Achievement ID>" << endl;
+    cout << "FriendsWhoPlay <Player ID> <Game ID>" << endl;
+    cout << "ComparePlayers <Player ID1> <Player ID2> <Game ID>" << endl;
+    cout << "SummarizePlayer <Player ID>" << endl;
+    cout << "SummarizeGame <Game ID>" << endl;
+    cout << "SummarizeAchievement <Game ID> <Achievement ID>" << endl;
+    cout << "AchievementRanking" << endl;
+}
+
+int main(){
+    
+    cout << "\nHello welcome to Acheivement Tracker" << endl;
+    cout << "To begin type in the command you wish to run with the correct credentials" << endl;
+    cout << "or type \"help\" to list the available commands." << endl;
+    
+    string input;
+    cin >> input;
+    if (input == "help") help();
+    
+    
+    
     return 0;
 }
 
